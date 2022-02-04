f9490bc0beaf7f84343b055207a53e9c6b8a3965
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-04 16:30:34 +0300

    templates and custom css

--- run.py
+++ run.py
@@ -8,13 +8,13 @@ posts = [
         'author': 'Corey Schafer',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'date_posted': 'April 20, 2022'
     },
     {
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'April 21, 2022'
     }
 ]
 
