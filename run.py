ac7aab99f1605b9b90b1dbefb95793ba37e4a9cf
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-05 14:54:34 +0300

    templating login form

--- run.py
+++ run.py
@@ -43,10 +43,16 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login ():
       form = LoginForm()
-      return render_template("login.html",title = "Login",form=form)
+      if form.validate_on_submit():
+       if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+        flash('You have been logged in!','success')
+        return redirect(url_for('home'))
+      else:
+        flash('Login unsuccessful.Please check username and password', 'danger')
+        return render_template("login.html",title = "Login",form=form)
    
    
   
