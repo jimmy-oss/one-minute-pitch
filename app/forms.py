e7f8957684b55081f5272326741bea2c37ee8841
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-06 14:13:41 +0300

    updating user profile pics size using pillow extension

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
@@ -14,18 +15,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-    def validate_username(self,username):
-         if username.data !=  current_user.username:
-           user = User.query.filter_by(username=username.data).first()
-           if user:
-               raise ValueError('That Username is Taken. Please choose a different one ')
-           
-    def validate_email(self,email):
-        if email.data !=  current_user.email:
-           user = User.query.filter_by(email=email.data).first()
-           if user:
-               raise ValueError('That Email is Taken. Please choose a different one ')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
@@ -34,20 +33,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-    
+
+
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
-    def validate_username(self,username):
-           user = User.query.filter_by(username=username.data).first()
-           if user:
-               raise ValueError('That Username is Taken. Please choose a different one ')
-           
-    def validate_email(self,email):
-           user = User.query.filter_by(email=email.data).first()
-           if user:
-               raise ValueError('That Email is Taken. Please choose a different one ')
 
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
