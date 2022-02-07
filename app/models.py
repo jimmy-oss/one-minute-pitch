e058c86e67358dc2ae1a05571817f1791fff5de9
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-07 11:07:50 +0300

    using pagination per page

--- app/models.py
+++ app/models.py
@@ -15,6 +15,8 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    
+    
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
