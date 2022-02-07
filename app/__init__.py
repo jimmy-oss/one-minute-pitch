af76c85faf8fa42d414dc82348f62141128d7f81
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-07 22:56:13 +0300

    using error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -33,10 +33,12 @@ def create_app(config_class=Config):
     from app.users.routes import users
     from app.posts.routes import posts
     from app.main.routes import main
+    from app.errors.handlers import errors
     
     # Registering the blueprint
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
