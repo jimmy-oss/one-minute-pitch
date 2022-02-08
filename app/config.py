a35ca62c00cc225505f46eeb771f1519b4dca2a6
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-08 19:48:18 +0300

    updates

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Config:
       '''
      
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
